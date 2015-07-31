061ff51bd9a618c0e17714705f378f6783f76686
Chuck <chkengr@gmail.com>
2015-07-31 15:31:43 -0400

    Implementation of Tile_theory objects too complicated... moving on
    Using a tile theory object results in complicated data structures
    with complicated logic.  In order to make things simpler,
    Edge_theory objects will be made.

--- minesweeper.py
+++ minesweeper.py
@@ -29,11 +29,11 @@ class Game_board(object):
     def print_board_numbers(self):
         print("##########Board Numbers###########")
         for i in range(0, self._y_dim):
+            print("row numbers " + str(self._y_dim))
+            print("column numbers " + str(self._x_dim))
             row_values = [self._tile_values[j, i]
                          for j in range(0, self._x_dim)]
             print(" ".join(str(a) for a in row_values ))
- 
- 
  
     def print_clicked_tiles(self):
         print("##########Clicked Tiles#########")
@@ -49,7 +49,6 @@ class Game_board(object):
             print(row)
         print("###############################")
         print("You have flagged " + str(len(self._flagged)))
- 
  
     def click_tile(self, location):
         if location in self._mined:
@@ -70,7 +69,8 @@ class Game_board(object):
         if win:
             print("you've won")
             self.game_over = True
- 
+    def get_mine_count(self):
+        return len(self._mined)
     def game_over(self):
         return self.game_over 
  
@@ -129,7 +129,63 @@ class Game_board(object):
     @y_dim.setter
     def y_dim(self, value):
         self._y_dim = value
- 
+
+class Text_generated_board(Game_board):
+    def __init__(self, text):
+        """
+            a board is generated using input text rather than 
+            dimensions and mine counts.  Each row is contained
+            in a string.
+
+            * = unflagged mine
+            # = flagged mine
+            1,2,...,8 = clicked tile
+            s = unclicked tile
+        """
+        rows = [] 
+        for r in text:
+            values_in_row = [a in r for a in "#*s012345678"]
+            if any(values_in_row) and not " " in r:
+                rows.append(r.strip())    
+
+        self._x_dim = len(rows[0])
+        self._y_dim = len(rows)
+
+        self._tile_values = dict() 
+        self._flagged = [] 
+        self._clicked = [] 
+        self._unclicked = [] 
+        self._mined = [] 
+        
+        row_count = 0
+        for r in rows:
+            column_count = 0
+            for c in list(r):
+                if c == '*':
+                    self._mined.append((column_count,row_count))
+                    self._unclicked.append((column_count, row_count))
+                elif c == '#':
+                    self._flagged.append((column_count,row_count))
+                elif c == 's':
+                    self._unclicked.append((column_count,row_count))
+                else:
+                    self._tile_values[column_count, row_count] = int(c)
+                    self._clicked.append((column_count, row_count))
+                column_count += 1
+            row_count += 1  
+
+        uncounted_spaces = self._mined + self._flagged + self._unclicked
+        for i in uncounted_spaces:
+            adjacent_mines = count_adjacent_group(i, self._mined)
+            self._tile_values[i] = adjacent_mines
+       
+        self._tile_coordinates = list(self._tile_values)
+        self._flagged = set(self._flagged)
+        self._clicked = set(self._clicked)
+        self._unclicked = set(self._unclicked)
+
+        self.game_over = False
+
 class Tile_theory(object):
     def __init__(self, unclicked, clicked_count, flagged_count, mine_count):
         uncounted_mines = mine_count - flagged_count
@@ -138,7 +194,6 @@ class Tile_theory(object):
         empty_spaces = len(unclicked) - uncounted_mines 
         empty = [0] * empty_spaces 
 
-
         combinations = set([a for a in permutations(mines + empty) 
                        if len(a) == len(unclicked)])
 
@@ -146,26 +201,22 @@ class Tile_theory(object):
         # tile locations each row is a possible situation
         self._possible_permutations = [{s: m for s,m in zip(unclicked, p)}
                                          for p in combinations]
-
-
-#        print("\n".join([ str(a) for a in self._possible_permutations]))
+    def print_permutations(self):
+        for i in self._possible_permutations:
+            print(" ".join([str(a)  for a in i.items()]))
 
     def check_permutations (self, reference_permutations):
-#        self._possible_permutations = [a for a in self._possible_permutations
-#                                      if any([permutation_agree(a, ref) 
-#                                     for ref in reference_permutations])]
         new_permutations = []
         for a in self._possible_permutations:
-            agreement = [permutation_agree(a, ref) 
-                         for ref in reference_permutations]
 
-            # testing if each possibility agrees with at least one of the 
-            # other theories.  otherwise, it is not included in the new list
-            if any(agreement):
+#            agreement = [permutation_agree(a, ref) 
+#                        for ref in reference_permutations]
+            agreement = permutation_agree(a, reference_permutations)
+            if agreement:
                 new_permutations.append(a)
-
         self._possible_permutations = new_permutations
-                                       
+        
+
     @property
     def possible_permutations(self):
         return self._possible_permutations
@@ -173,7 +224,6 @@ class Tile_theory(object):
     @possible_permutations.setter
     def possible_permutations(self, value):
         self._possible_permutations = value
- 
 
 def permutation_agree(test_permutation, reference_permutation):
     """
@@ -181,27 +231,26 @@ def permutation_agree(test_permutation, reference_permutation):
         as the keys and with the mine values as the value (0 or 1).
         If they disagree on at least one value, this function
         returns a false
-    """ 
+    """
+    permutation_set = set(test_permutation)
+    reference_set = set(reference_permutation)
+
+    intersection = permutation_set.intersection(reference_set)
+    overlap = len(intersection) != 0
+    
     # if none of the dictionary keys match, the permutations can not disagree,
     # so the intersection of the two sets is checked.  If there is no overlap,
     # then the permutations agree (so true is returned).
-    permutation_set = set(test_permutation)
-    reference_set = set(reference_permutation)
-    intersection = permutation_set.intersection(reference_set)
-    overlap = len(intersection) != 0
     if not overlap:
         return True
 
-    any_disagreement = [False for a in intersection
-                       if test_permutation[a] != reference_permutation[a]]
+    conflict_free = all([test_permutation[a] == reference_permutation[a] 
+                    for a in intersection])
 
-    if any_disagreement:
-        return False
+    if conflict_free:
+        return True
     else:
-        return True  
-
-
-
+        return False
 
 
 def get_adjacent_tiles(point_location, group_locations):
@@ -241,9 +290,14 @@ def decide(board):
         for i in board.clicked:
             adjacent_unclicked = count_adjacent_group(i, board.unclicked) 
             cell_complete = adjacent_unclicked == 0
+
             if not cell_complete:
                 adjacent_mines = board.tile_values[i]
                 unclicked_tiles =  get_adjacent_tiles(i, board.unclicked)
+
+                if adjacent_mines == 0:
+                    for a in unclicked_tiles:
+                        board.click_tile(a)
                 adjacent_flagged = count_adjacent_group(i, board.flagged)
                 adjacent_clicked = count_adjacent_group(i, board.clicked) 
                 
@@ -257,19 +311,55 @@ def decide(board):
                     for a in unclicked_tiles:
                         board.flag_tile(a)
                     return 
- 
+    probability_guess(board) 
     # if none of that works, just guess               
-    guess(board)
- 
-def guess(board):
-    random_pick = random.choice(list(board.unclicked))
-    board.click_tile(random_pick)
+
+def probability_guess(board):
+    # getting rid of complete tiles since there are no adjacent
+    # tiles next to them
+    choices = [a for a in board.clicked
+              if count_adjacent_group(a, board.unclicked) != 0]
     
-         
- 
-def solve(board, mine_number):
+    theories = []
+    for a in choices:
+        unclicked = get_adjacent_tiles(a, board.unclicked)
+        clicked_count = count_adjacent_group(a, board.clicked)
+        flagged_count = count_adjacent_group(a, board.flagged)
+        mine_count = board.tile_values[a]
+        theories.append(Tile_theory(unclicked, clicked_count, 
+                        flagged_count, mine_count))
+    for i in theories:
+        i.print_permutations()
+
+    for a in theories:
+        for b in theories:
+            for c in b.possible_permutations: 
+                a.check_permutations(c)
+        
+        if len(a.possible_permutations) == 1:
+            for d in a.possible_permutations[0]:
+                if a.possible_permutations[0][d] == 1:
+                    board.flag_tile(d)
+                else:
+                    board.click_tile(d)
+            return
+
+    for i in theories:
+        i.print_permutations()
+
+
+
+    guess(board) 
+
+def guess(board):
+    if len(board.unclicked) > 0:
+        random_pick = random.choice(list(board.unclicked))
+        board.click_tile(random_pick)
+     
+def solve(board):
+    mine_number = board.get_mine_count()
     while not board.game_over:
-#        time.sleep(0.1)
+        time.sleep(0.1)
         previous_unclicked = len(board.unclicked)
  
         decide(board)
@@ -278,17 +368,28 @@ def solve(board, mine_number):
  
  
     if len(board.flagged) == mine_number:
+        print("\n".join([str(a) for a in board.flagged]))
         print("You've won, congratulations")
     else:
         print("Better luck next time")
         print("you only had %i mines left" % (mine_number - len(board.flagged)))
-     
+
  
 if __name__ == "__main__":
-    for i in range(0, 10): 
-        BOARD_DIMENSIONS = (30, 16)
-        MINE_NUMBER = 99
+    # running 2 simulations
+    for i in range(0, 1): 
+        
+        BOARD_DIMENSIONS = (16, 16)
+        MINE_NUMBER = 4
+        board_rows = ['ssssssssss',
+                      'ss*sss*sss',
+                      'ssssssss*s',
+                      'ss*sssssss']  
+        board_rows = ['*s*','121']
+        print(board_rows)
+        my_board = Text_generated_board(board_rows)
         my_board = Game_board(BOARD_DIMENSIONS, MINE_NUMBER)
-        solve(my_board, MINE_NUMBER)
+        
+        solve(my_board)
      
  
--- test_mine.py
+++ test_mine.py
@@ -2,8 +2,10 @@
 
 import unittest
 
-from minesweeper import get_adjacent_tiles, count_adjacent_group, permutation_agree
-from minesweeper import Tile_theory
+import minesweeper
+from minesweeper import get_adjacent_tiles, count_adjacent_group
+from minesweeper import Tile_theory, permutation_agree
+
 
 class my_test (unittest.TestCase):
     def test_get_adjacent_tiles(self):
@@ -22,16 +24,51 @@ class my_test (unittest.TestCase):
     def test_permutations_agree(self):
         reference_permutation = {(1,2):1, (3,3):0, (1,1):1}
         test_permutation = {(1,2):0, (1,1):1, (2,2):0}
+
+        test_theory = Tile_theory( {(2,3),(3,3),(4,3)}, 5, 0, 2) 
         agreement = permutation_agree(test_permutation, reference_permutation)
         self.assertTrue(not agreement)
 
+        reference_permutation = {(1,0):0, (0,0):1}
+        test_permutation = {(1,0):1, (2,0):0, (0,0):1}
+        
+        agreement = permutation_agree(test_permutation, reference_permutation)
+        self.assertTrue(not agreement)
+
+        reference_permutation = {(1,0):1, (0,0):1}
+        test_permutation = {(1,0):1, (2,0):0, (0,0):1}
+        agreement = permutation_agree(test_permutation, reference_permutation)
+        self.assertTrue(agreement)
+
+
+
+        
     def test_tile_theory(self):
-        test_theory = Tile_theory( {(2,3),(3,3),(4,3)}, 5, 0, 2) 
+        """
+            Testing this situation
+           ##   ~   ~ = wall
+            4   ~   # = flag
+            #321~
+            #200~
+            ~~~~~
+
+            Looking at the area around the 3 and the
+            2 to its right 
+        """
         other_theory = Tile_theory( {(2,3), (3,3)}, 3, 2, 3)
+        test_theory = Tile_theory( {(2,3),(3,3),(4,3)}, 5, 0, 2) 
 
         test_theory.check_permutations(other_theory.possible_permutations)
-
         self.assertEqual(2, len(test_theory.possible_permutations))
+
+        # example
+        # *s*
+        # 121
+        board1 = ['*s*','121']
+        board = minesweeper.Text_generated_board(board1)
+        test_theory = Tile_theory(board.unclicked, len(board.clicked),
+                                  len(board.flagged), board.get_mine_count())
+        
  
 if __name__ == "__main__": 
     unittest.main()
